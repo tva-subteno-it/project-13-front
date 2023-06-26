9693c7aa56c459a853a1b5f9ae5259cb08c129cd
Timothée Vannier <tva@subteno.com>
2023-06-26 12:15:11 +0200

    [IMP] all pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from "./pages/home";
+import Root from "./components/Root";
+import SignIn from "./pages/sign-in";
+import User from "./pages/user";
+import NotFound from "./pages/404/404.tsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>
+        element: <Root/>,
+        errorElement: <NotFound/>,
+        children: [
+            {path: '/', element: <Home/>},
+            {path: '/sign-in', element: <SignIn/>},
+            {path: 'user', element: <User/>}
+        ]
     }
 ])
 
